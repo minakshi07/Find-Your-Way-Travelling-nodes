de1cae2a44f4bfc4f1a80c54f091d0b26c6f28f6
Shashank Gupta <shashankgupta10ajbl@gmail.com>
2019-11-06 21:55:51 +0530

    Comments updated for master branch
    Comments for homepage UI, detailing navbar, dropdown menus and loaded scripts.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Importing express modules to host on local server.
 const express = require("express");
 const app = express();
 
 app.use("/public", express.static(__dirname + "/public"))
 
+//Importing initial homepage.
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
+
 app.listen(1337, () => {
   console.log("The server is up and running!");
 });
+//Have used nodemon plugin to restart server at each save.
